eebd0f3af666becc2ac4ab4d136540c695fb9ae0
arfpchatelot <fchatelot@yahoo.fr>
2024-01-04 11:24:55 +0100

    Ajout de la finalisation du TP Colblanc avec rech par type etablissement

--- colblanc/index.php
+++ colblanc/index.php
@@ -6,6 +6,7 @@
   <title>Entrainement Centre de Readaptation</title>
   <link rel="stylesheet" media="screen" href="css/style.css">
   <link rel="stylesheet" media="print" href="css/style2.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
@@ -100,8 +101,18 @@
           <fieldset>
             <legend>Sélectionner le type d'établissement</legend>
             <div>
-              <input type="checkbox" name="choix[]" id="tpe" value="TPE">
+
+              <input type="checkbox" name="choix[]" id="tpe" value="TPE" <?php
+                                                                          if (isset($_POST["choix"])) {
+
+                                                                            foreach ($_POST["choix"] as $key =>  $value) {
+                                                                              if ($value == "TPE") {
+                                                                                echo "checked='true'";
+                                                                              }
+                                                                            }
+                                                                          } ?> />
               <label for="tpe"> TPE</label>
+
             </div>
             <div>
               <input type="checkbox" name="choix[]" id="pme" value="PME">
@@ -139,34 +150,63 @@
       </section>
 
 
+
+
+      <?php
+
+      // var_dump($_POST["choix"]);
+
+      // var_dump($_POST);
+      $finrq = "";
+      if (isset($_POST["choix"]) && count($_POST["choix"]) > 0) {
+        $liste = "";
+        for ($i = 0; $i < count($_POST["choix"]); $i++) {
+
+          $liste .= ",'" . $_POST["choix"][$i] . "'";
+        }
+        $liste = substr($liste, 1);
+
+
+        $finrq = " AND type_etab IN(" . $liste . ")";
+      }
+
+
+
+      $connect = Connexion::getinstance();
+      $rq = "SELECT nom_etab, type_etab, nom_resp, adresse, ville, cp, Telephone, email FROM institutions WHERE depart=:departement " . $finrq;
+      $state = $connect->prepare($rq);
+      $state->bindParam(":departement", $_POST["dep"], PDO::PARAM_STR);
+      $state->execute();
+      $data = [];
+      $nbEntr = 0;
+
+      echo "<caption > Resultats de votre recherche </caption><table class='table table-striped table-hover'>";
+      echo "<thead><tr><th>Nom etablissement</th> <th> type etab</th> <th> Nom resp</th> <th>adresse</th><th>code postal</th><th>ville</th><th>Tél</th><th>mail</th></tr></thead><tbody>";
+
+
+      while ($obj = $state->fetch()) {
+        echo "<tr>";
+        $nbEntr++;
+        array_push($data, $obj);
+        foreach ($obj as $key => $value) {
+          echo '<td> ' . utf8_decode($obj->$key) . '</td>';
+        }
+        echo "</tr>";
+      }
+      //var_dump($data);
+      echo "</tbody></table>";
+
+      ?>
     </main>
-
-    <?php
-
-    // var_dump($_POST["choix"]);
-
-    // var_dump($_POST);
-
-    $connect = Connexion::getinstance();
-    $rq = "SELECT nom_etab, type_etab, nom_resp, adresse, ville, cp, Telephone, email FROM institutions WHERE depart=:departement";
-    $state = $connect->prepare($rq);
-    $state->bindParam(":departement", $_POST["dep"], PDO::PARAM_STR);
-    $state->execute();
-    $data = [];
-    $nbEntr = 0;
-    while ($obj = $state->fetch()) {
-      $nbEntr++;
-      array_push($data, $obj);
-    }
-    var_dump($data);
-
-
-    ?>
-
     <footer>
       Copyright
     </footer>
+
   </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 
 </html>
